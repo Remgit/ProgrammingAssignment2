be009f4cdf7096f0de2f523aceb1e5a2ec6db6ab
Remgit <httandogan@yahoo.com>
2015-01-22 15:32:35 +0100

    Assingment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# STEP 1: The first function, makeCacheMatrix creates a special "Matrix", 
+# which is really a list containing a function to
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the Inverse
+## 4.get the value of the Inverse
 
+makeCacheMatrix <- function(x = matrix()) {
+        I <- NULL               # Changing m to I as "inverse" and setting it to NULL
+        set <- function(y){
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x     
+        setInverse <- function(solve) I <<- solve  # Setting inverse of x with "solve" function by replacing "mean"
+        getInverse <- function() I
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+# STEP 2: The following function calculates the Inverse of the special "vector" created with the above function. 
+# However, it first checks to see if the mean has already been calculated. 
+# If so, it gets the mean from the cache and skips the computation. 
+# Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getInverse()
+        if(!is.null(I)){
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setInverse(I)
+        I 
 }
